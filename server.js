006f3e6d696d5a262017fa634d92966d1cc1ffba
KaitoKid-123 <92264855+KaitoKid-123@users.noreply.github.com>
2023-04-17 02:15:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const server = app.listen(PORT, console.log(`Server Started on port ${PORT}`)); 
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://frontend-ghdt.onrender.com",
     },
 })
 
